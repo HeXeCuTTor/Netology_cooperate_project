0479fe6ff2d61fc76e2fbdf257b1cf26caaed5b7
Ilya Voloshin <ilyavoloshin20@gmail.com>
2023-01-23 18:20:46 +0700

    Third commit

--- VK_download.py
+++ VK_download.py
@@ -60,8 +60,8 @@ class VK:
                         for key, value in list.items():
                             if key == 'likes':
                                 likes = value.get('count')
-                            if key == 'sizes':
-                                photo_data['url'] = value[-1].get('url')      
+                            if key == 'id':
+                                photo_data['photo_id'] = value      
                         photo_data['likes'] = likes
                         photos_json.append(photo_data)
                         photo_data = {}
@@ -74,9 +74,9 @@ class VK:
         self.age_to = age_to
         self.city_id = get_city
         url = 'https://api.vk.com/method/users.search'
-        params_1 = {'sort': 1, 'sex':self.sex, 'count': 10, 'fields': ['bdate'], 'city_id':self.city_id,'has_photo': 1, 'status': 1, 'age_from': self.age_from, "age_to": self.age_to, 'can_access_closed': 1}
+        params_1 = {'sex':self.sex, 'count': 10, 'fields': ['bdate'], 'city_id':self.city_id,'has_photo': 1, 'status': 1, 'age_from': self.age_from, "age_to": self.age_to, 'can_access_closed': 1}
         response_1 = requests.get(url, params={**self.params, **params_1}).json()
-        params_6 = {'sort': 1, 'sex':self.sex, 'count': 10, 'fields': ['city','bdate'], 'city_id':self.city_id,'has_photo': 1, 'status': 6, 'age_from': self.age_from, "age_to": self.age_to, 'can_access_closed': 1}
+        params_6 = {'sex':self.sex, 'count': 10, 'fields': ['city','bdate'], 'city_id':self.city_id,'has_photo': 1, 'status': 6, 'age_from': self.age_from, "age_to": self.age_to, 'can_access_closed': 1}
         response_6 = requests.get(url, params={**self.params, **params_6}).json()
         def free_profile(response):
             users_data = {}
@@ -101,13 +101,13 @@ class VK:
                                         users_data['first_name'] = name
                                         users_data['last_name'] = surname
                                         users_data['bdate'] = birthday
-                                        users_data['link'] = f'https://vk.com/id{user_id}'
+                                        users_data['user_id'] = user_id
                                         photos = vk.users_get_photo(user_id)
                                         num = 0                                    
                                         if photos != []:
                                             for photo in photos:
                                                 for keys, values in photo.items():
-                                                    if keys == 'url':
+                                                    if keys == 'photo_id':
                                                         num += 1
                                                         users_data[f'{keys} {num}'] = values
                                             users_dates.append(users_data)
@@ -119,12 +119,12 @@ class VK:
     
 
 if __name__ == '__main__':
-    # country = "Россия"
-    # region = "Новосибирская область"
-    # city = "Новосибирск"
-    # sex = '1'
-    # age_from = '18'
-    # age_to = '30'
+    country = "Россия"
+    region = "Новосибирская область"
+    city = "Новосибирск"
+    sex = '1'
+    age_from = '18'
+    age_to = '30'
     vk = VK(token_VK)
     get_country = vk.get_id_countries(country)
     get_city = vk.get_id_city(get_country, region, city)
